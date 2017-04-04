1cf1a3fc11f645d47c6c48ef8439b88bbd4da031
tmulroy <thomasjohnmulroy@gmail.com>
2017-04-04 13:49:33 -0400

    add whyDidYouUpdate to <App />

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,11 @@
 import React, { PureComponent, PropTypes } from 'react';
 
+if (process.env.NODE_ENV !== 'production') {
+  const {whyDidYouUpdate} = require('why-did-you-update')
+  whyDidYouUpdate(React)
+}
 export default class App extends PureComponent {
+
   render() {
     return (
       <div>
