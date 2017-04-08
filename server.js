ee0eb725b5389bac2839a23141756b3dc3a0c13f
tom <thomasjohnmulroy@gmail.com>
2017-04-07 22:02:52 -0400

    local changes

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const path = require('path'),
       app = express(),
       ONE_YEAR = 31536000000,
       tlsOptions = {
-        key: fs.readFileSync(`${process.env.KEY_PATH}`),
-        ca: fs.readFileSync(`${process.env.CA_PATH}`),
-        cert: fs.readFileSync(`${process.env.CERT_PATH}`),
+        key: fs.readFileSync(process.env.KEY_PATH),
+        ca: fs.readFileSync(process.env.CA_PATH),
+        cert: fs.readFileSync(process.env.CERT_PATH),
       };
 
 
@@ -58,11 +58,15 @@ app.get('/', (req, res) => {
 
 // uncomment when there's a HTTP server and a redirect to HTTPS server
 // http.createServer(app).listen(80);
-app.get('')
-app.get('/api',  (req, res) => {
-  res.send('hello from https api (not authenticated)')
-});
+app.post('/login',
+  passport.authenticate('local'),
+  (req, res) => {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
 // Start HTTPS server
-https.createServer(tlsOptions, app).listen(process.env.PORT, () => {
+https.createServer(tlsOptions, app).listen(process.env.PORT,() => {
   console.log(`Secure Server on ${process.env.PORT}`);
 });
